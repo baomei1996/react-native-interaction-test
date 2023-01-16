1f932c59cf5470a59429e3e958da4d616ead2e8f
baomei1996 <qhal4686@gmail.com>
2023-01-17 00:22:10 +0900

    Feat: rotateY transform origin x: 0, y: 0 맞추어 처리

--- src/screens/RotateBoxTest.jsx
+++ src/screens/RotateBoxTest.jsx
@@ -8,20 +8,45 @@ function RotateBoxTest() {
   const entering = targetValues => {
     'worklet';
     const animations = {
-      transform: [{rotateY: withTiming('0deg', {duration: 2000})}],
+      opacity: withTiming(1, {duration: 300}),
+      transform: [
+        {translateX: withTiming(-200 / 2, {duration: 400})},
+        {rotateY: withTiming('0deg', {duration: 400})},
+        {translateX: withTiming(200 / 2, {duration: 400})},
+      ],
     };
     const initialValues = {
-      transform: [{rotateY: '-90deg'}],
+      opacity: 0,
+      transform: [
+        {translateX: -200 / 2},
+        {rotateY: '-80deg'},
+        {translateX: 200 / 2},
+      ],
     };
     return {
       initialValues,
       animations,
     };
   };
+
   const exiting = targetValues => {
     'worklet';
-    const animations = {};
-    const initialValues = {};
+    const animations = {
+      opacity: withTiming(0, {duration: 300}),
+      transform: [
+        {translateX: withTiming(-200 / 2, {duration: 400})},
+        {rotateY: withTiming('-80deg', {duration: 400})},
+        {translateX: withTiming(200 / 2, {duration: 400})},
+      ],
+    };
+    const initialValues = {
+      opacity: 1,
+      transform: [
+        {translateX: -200 / 2},
+        {rotateY: '0deg'},
+        {translateX: 200 / 2},
+      ],
+    };
     return {
       initialValues,
       animations,
